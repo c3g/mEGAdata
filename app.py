c7900d9a377a616ef9cbcfcddd1ecaa65665138a
dbujold <david.bujold@mail.mcgill.ca>
2016-03-21 18:30:31 -0400

    Removed Stormpath imports for now.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 import peewee
-from flask.ext.stormpath import StormpathManager
+#from flask.ext.stormpath import StormpathManager
 
 app = Flask(__name__)
 app.config.from_object('settings')
-stormpath_manager = StormpathManager(app)
+#stormpath_manager = StormpathManager(app)
 
 db = peewee.MySQLDatabase(app.config["DATABASE_NAME"],
                           host=app.config["DATABASE_HOST"],
