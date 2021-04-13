613f90eec55778827faa8ca03fe29bb982e5cfa0
David Brownlee <david.brownlee@c3g.ca>
2021-04-13 09:33:02 -0400

    dataset_to_release_set info added.  Initial generation of relationMapping.ods.

--- migration/2.0.5_to_importSpreadsheetData.sql
+++ migration/2.0.5_to_importSpreadsheetData.sql
@@ -13,3 +13,8 @@ INSERT INTO experiment_property(property, type, is_exported_to_ega) VALUES ('pre
 
 -- New ChIP-Seq experiment_properties
 INSERT INTO experiment_property(property, type, is_exported_to_ega) VALUES ('experiment_target_histone', 'text', 1);
+
+-- New release_set (aka EGA datasets) for R7
+INSERT INTO release_set(`release`, `name`, `description`) VALUES ('R7', 'McGill EMC Release 7 for EMC Community_1 - Brent Richards', 'McGill EMC Release 7 for EMC Community_1 - Brent Richards');
+INSERT INTO release_set(`release`, `name`, `description`) VALUES ('R7', 'McGill EMC Release 7 for EMC Community_2 - Noel Raynal', 'McGill EMC Release 7 for EMC Community_2 - Noel Raynal');
+INSERT INTO release_set(`release`, `name`, `description`) VALUES ('R7', 'McGill EMC Release 7 for EMC Community_3 - Jacques Cote', 'McGill EMC Release 7 for EMC Community_3 - Jacques Cote');
---
+++ scripts/EGA_json_submitter/generate_relation_mapping.py
@@ -0,0 +1,55 @@
+# -*- coding: future_fstrings -*-
+import os
+import sys
+
+# Make models.py accessible in parent dir.
+SCRIPTS_ROOT_DIR = os.path.dirname(os.getcwd())
+sys.path.append(SCRIPTS_ROOT_DIR)
+
+import peewee
+from models import Dataset, Sample, ExperimentType, Run, RunFile, DatasetToReleaseSet, ReleaseSet
+
+# Generate a relationMapping.ods from the mEGAdata DB, for a given dataset.release_status (R1, R2, R3, etc.)
+# This relationMapping.ods is an unnormalizion of the dataset, sample, experiment, run and run_files tables and serves as an input for submissions to EGA.
+
+def main():
+    # Write all that header info.
+    f = open("out.csv", "w")
+    # Spreadsheet Title
+    f.write("EMC Community - Unnormalized\n")
+    # EGA Object headers
+    f.write("Dataset,,,Sample,,,Experiment,,,Run,,,Files\n")
+    # Column headers
+    f.write("DatasetAlias,ID,EGAD,SampleAlias,ID,EGAN,ExperimentAlias,ID,EGAX,RunAlias,ID,EGAR,File1_Alias,File1_fileName,File1_Checksum,File1_Encrypted_Checksum,File1_ID,File1_EGAF,File2_Alias,File2_fileName,File2_Checksum,File2_Encrypted_Checksum,File2_ID,File2_EGAF\n")
+
+    # Query Dataset, Sample and ExperimentType and Run info
+    query = Dataset.select(Dataset, Sample, ExperimentType, Run)\
+        .join(Sample)\
+        .switch(Dataset).join(ExperimentType)\
+        .switch(Dataset).join(Run)\
+        .where((Dataset.release_status == "R7") ) # & (Dataset.id == 5072))
+    results = query.execute()
+    for ds in results:
+        # Query and write the release_set (aka, the EGA Dataset)
+        dtrs_query = DatasetToReleaseSet.select(DatasetToReleaseSet, ReleaseSet, Dataset)\
+            .join(ReleaseSet)\
+            .switch(DatasetToReleaseSet)\
+            .join(Dataset).where(Dataset.id == ds.id)
+        dtrs_results = dtrs_query.execute()
+        for dtrs in dtrs_results:
+            f.write(f"{dtrs.release_set.name},,,")
+
+        # Write Dataset, Sample and ExperimentType and Run info
+        f.write(f"{ds.sample.public_name},,,{ds.experiment_type.internal_assay_short_name},,,{ds.run.library_name},,,")
+
+        # Query and write the two run_files, to be written on the same line.
+        run_file_query = RunFile.select(RunFile).where(RunFile.run_id == ds.run.id)
+        run_file_results = run_file_query.execute()
+        for rf in run_file_results:
+            f.write(f"{rf.name},{rf.name},{rf.md5},{rf.encrypted_md5},,,")
+        f.write("\n")
+    f.close()
+
+
+if __name__ == "__main__":
+  main()
